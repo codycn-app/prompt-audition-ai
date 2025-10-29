461c223c329435010c0710c10a5fa6ab719bc2a1
codycn-app <codycn2804@gmail.com>
2025-10-29 20:11:38 +0700

    fixx

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -15,7 +15,7 @@ const ImageCard: React.FC<ImageCardProps> = ({ image, onClick, currentUser }) =>
 
   return (
     <div 
-      className="relative overflow-hidden transition-all duration-300 ease-in-out border-2 rounded-xl cursor-pointer group bg-cyber-surface border-cyber-surface/50 hover:shadow-cyber-glow-lg hover:-translate-y-1 hover:border-cyber-pink/80"
+      className="relative overflow-hidden transition-all duration-300 ease-in-out border-2 rounded-xl cursor-pointer group bg-cyber-surface border-cyber-surface/50 hover:shadow-cyber-glow-lg hover:-translate-y-1 hover:border-cyber-pink/80 mb-6"
       onClick={onClick}
       style={{ breakInside: 'avoid' }}
     >
