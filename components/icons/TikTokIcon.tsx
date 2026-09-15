7c0a6ba56d2ec2d9faab061eba642fac4bae3fee
codycn-app <cuongnguyen28041997@gmail.com>
2026-09-16 01:34:30 +0700

    123

---
+++ components/icons/TikTokIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const TikTokIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+    <path d="M16.72 5.03A4.69 4.69 0 0 1 14.4 2h-3.16v12.17a2.8 2.8 0 1 1-2.8-2.8c.22 0 .43.03.63.08V8.24a6.02 6.02 0 1 0 5.33 5.98V8.05a7.82 7.82 0 0 0 4.58 1.48V6.37a4.66 4.66 0 0 1-2.26-1.34Z" />
+  </svg>
+);
